854483defd5cf49e3ebc848a6c5cadce08f01376
Thomas Slade <me@thomas-slade.com>
2018-07-20 10:11:51 -0400

    Added vscode update command

---
+++ .zcustom/apps.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+vs-code-update() {
+  wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O /tmp/code_latest_amd64.deb
+  sudo dpkg -i /tmp/code_latest_amd64.deb
+}
