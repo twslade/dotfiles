b3d50408700883871efe027a5e92d14a57381202
Thomas Slade <me@thomas-slade.com>
2018-09-20 09:13:58 -0400

    Added php7.2

--- .zcustom/php.zsh
+++ .zcustom/php.zsh
@@ -1,21 +1,28 @@
 php-56(){
 	sudo update-alternatives --set php /usr/bin/php5.6
-	sudo a2dismod php7.1 php7.0
+	sudo a2dismod php7.1 php7.0 php7.2
 	sudo a2enmod php5.6
-	sudo service apache2 restart
-}
-
-php-71(){
-	sudo update-alternatives --set php /usr/bin/php7.1
-	sudo a2dismod php5.6 php7.0
-	sudo a2enmod php7.1
 	sudo service apache2 restart
 }
 
 php-70(){
 	sudo update-alternatives --set php /usr/bin/php7.0
-	sudo a2dismod php5.6 php7.1
+	sudo a2dismod php5.6 php7.1 php7.2
 	sudo a2enmod php7.0
+	sudo service apache2 restart
+}
+
+php-71(){
+	sudo update-alternatives --set php /usr/bin/php7.1
+	sudo a2dismod php5.6 php7.0 php7.2
+	sudo a2enmod php7.1
+	sudo service apache2 restart
+}
+
+php-72(){
+	sudo update-alternatives --set php /usr/bin/php7.2
+	sudo a2dismod php5.6 php7.1 php7.0
+	sudo a2enmod php7.2
 	sudo service apache2 restart
 }
 
