566fec50d20175e8786d68a835f626b123d5d25b
Thomas Slade <me@thomas-slade.com>
2019-03-05 08:07:17 -0500

    History defaults

--- .custom/history.zsh
+++ .custom/history.zsh
@@ -1,4 +1,23 @@
+
 # History Settings
+zshaddhistory() { whence ${${(z)1}[1]} >| /dev/null || return 1 } # Prevent failed commands from being saved
 
 HISTSIZE=1000000                   # The maximum number of events to save in the internal history.
 SAVEHIST=1000000                 # The maximum number of events to save in the history file.
+
+setopt NO_BG_NICE # don't nice background tasks
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions ???
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt IGNORE_EOF
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
